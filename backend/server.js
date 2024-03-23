53a369a4d4d47e7dbef850227d789e05bfa8ebe2
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-03-23 18:45:21 +0100

    feat: cors policy added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
+import cors from "cors"
 
 const port = process.env.PORT || 5000;
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+app.use(cors())
 
 app.use('/api/users', userRoutes);
 
